f6cf539c26d7bb37a799ba5f0f2fa67ffe35e8bc
Jjaloladdin <126801120+Jjaloladdin@users.noreply.github.com>
2023-05-31 16:37:59 +0500

    veb dasturlash m

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/news', function () {
+    return view('news');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
